3044492d014545eca8575b9e87f8e4f2d1b6f341
Sho Kuniyuki <79742417+shokuniyuki@users.noreply.github.com>
2023-03-16 22:05:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,26 @@
-const inputDate = document.getElementById("inputDate");
-const calculateBtn = document.getElementById("calculateBtn");
-const outputDiv = document.getElementById("output");
+const dates = [
+  ["B1 Framework Due", new Date("2023-04-01T00:00:00Z")],
+  ["B1 Deliveries Due", new Date("2023-04-22T00:00:00Z")],
+  ["B2 Framework Due", new Date("2023-05-13T00:00:00Z")],
+  ["B2 Deliveries Due", new Date("2023-06-03T00:00:00Z")],
+  ["B3 Framework Due", new Date("2023-06-24T00:00:00Z")],
+  ["B3 Deliveries Due", new Date("2023-07-15T00:00:00Z")],
+];
 
-function getWeekday(date) {
-  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  return weekdays[date.getDay()];
+const labels = ["B1 Framework Due: ", "B1 Deliveries Due: ", "B2 Framework Due: ", "B2 Deliveries Due: ", "B3 Framework Due: ", "B3 Deliveries Due: "];
+
+let datesOutput = '';
+
+for (let i = 0; i < dates.length; i++) {
+  const date = dates[i][1];
+  const label = labels[i];
+  const now = new Date();
+
+  if (now < date) {
+    const diffInMs = date.getTime() - now.getTime();
+    const diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
+    datesOutput += `<p>${label}${date.toLocaleDateString()} (${diffInDays} days)</p>`;
+  }
 }
 
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
-
-calculateBtn.addEventListener("click", function() {
-  // Get the input date value
-  const dateValue = inputDate.value;
-  
-  // Calculate the first set of dates
-  const date1 = new Date(dateValue);
-  date1.setDate(date1.getDate() + 45);
-  const date1Weekday = getWeekday(date1);
-  
-  const date2 = new Date(date1);
-  date2.setDate(date2.getDate() + 30);
-  const date2Weekday = getWeekday(date2);
-  
-  const date3 = new Date(date2);
-  date3.setDate(date3.getDate() + 30);
-  const date3Weekday = getWeekday(date3);
-  
-  // Calculate the second set of dates
-  const date4 = new Date(dateValue);
-  date4.setDate(date4.getDate() + 21);
-  const date4Weekday = getWeekday(date4);
-  
-  const date5 = new Date(date4);
-  date5.setDate(date5.getDate() + 7);
-  const date5Weekday = getWeekday(date5);
-  
-  // Calculate the third set of dates
-  const date6 = new Date(date2);
-  date6.setDate(date6.getDate() + 21);
-  const date6Weekday = getWeekday(date6);
-  
-  const date7 = new Date(date6);
-  date7.setDate(date7.getDate() + 7);
-  const date7Weekday = getWeekday(date7);
-  
-  // Display the results
-  outputDiv.innerHTML = `
-    <h2>Results</h2>
-    <ul>
-      <li>${formatDate(date1)} (${date1Weekday})</li>
-      <li>${formatDate(date2)} (${date2Weekday})</li>
-      <li>${formatDate(date3)} (${date3Weekday})</li>
-      <li>${formatDate(date4)} (${date4Weekday})</li>
-      <li>${formatDate(date5)} (${date5Weekday})</li>
-      <li>${formatDate(date6)} (${date6Weekday})</li>
-      <li>${formatDate(date7)} (${date7Weekday})</li>
-    </ul>`;
-});
+document.getElementById("dates").innerHTML = datesOutput;
